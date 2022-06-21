bd8f498f8616fd095fc3ef6dfe68224343d831a4
ChristianCammareri <38110773+ChristianCammareri@users.noreply.github.com>
2022-06-21 15:22:52 +0200

    Add files via upload

---
+++ marcolinWebToLead/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3010
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
